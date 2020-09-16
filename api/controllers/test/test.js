7bfc90284f9173139a2eed6103e37f445ee3aafd
X-vaylyne-O <brookeecobb@gmail.com>
2020-09-16 14:37:20 -0700

    v3

--- api/controllers/test/test.js
+++ api/controllers/test/test.js
@@ -1,5 +1,5 @@
 async function test(req, res, next) {
-	return res.status(200).json({ message: 'It Worked Version 2.0!' });
+	return res.status(200).json({ message: 'It Worked Version 3.0!' });
 }
 
 module.exports = test;
